1beacfd5150b9a5ec7442de7e9db6e4f6233246c
Mamont78 <mamont7886@gmail.com>
2017-08-21 19:50:16 +0300

    initial commit
    Change-Id: I8f6bef14788ef0a85a9c72f9dee18e7474489e8b

--- Data.cpp
+++ Data.cpp
@@ -1,23 +1,6 @@
 #include "Data.h"
 
-
-
-Data::Data(int size_data)
+void  DataLog::Data::data_push(int symbol)
 {
-	data.reserve(size_data);
-}
-
-void Data::data_path_in()
-{
-	std::cout << "Write the full path to the log" << std::endl;
-	std::cin >> path;
-}
-
-void Data::data_size_in(int size)
-{
-	data.reserve(size);
-}
-std::string Data::data_path_out()
-{
-	return path;
+	data.push_back(symbol);
 }
