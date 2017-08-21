37fc53c9b6e8c2cfafead7b04898e8a9f21faa31
Mamont78 <mamont7886@gmail.com>
2017-08-21 19:50:16 +0300

    initial commit
    Change-Id: I8f6bef14788ef0a85a9c72f9dee18e7474489e8b

---
+++ DfSettings.h
@@ -0,0 +1,63 @@
+#include <string>
+#include <iostream>
+#include <memory>
+
+#ifndef DFSETTINGS_H
+#define DFSETTINGS_H
+
+namespace Inst
+{
+	class Factory; //патерн фабрика
+
+	class ISettings
+	{
+	public:
+		virtual ~ISettings() {};
+		virtual std::string get_path() const = 0; //виртуальную функцию get_path() чисто обстрактную = 0
+	};
+
+	class Consol_Settings : public ISettings
+	{
+	public:
+		std::string get_path() const override; //получаем адрес к данным
+		friend class Factory; // даем доступ к private классу Factory
+
+	private:
+		Consol_Settings();
+		Consol_Settings(Consol_Settings const&) = delete; //копирование
+		Consol_Settings& operator=(Consol_Settings const&) = delete; // копирование 
+
+		std::string path; 
+	};
+
+	class Constans_Settings : public ISettings
+	{
+	public:
+		std::string get_path() const override;
+
+	private:
+		std::string path = "D:\\test.log";
+	};
+
+	class Factory
+	{
+	public:
+		enum Fabric_Type //перечесление
+		{
+			DEBUG = 0,
+			RELISE
+		};
+
+		Factory(Fabric_Type);
+		ISettings * Get_Settings()
+		{
+			return _Setting.get();
+		};
+
+	private:
+		std::unique_ptr <ISettings> _Setting;
+};
+}
+
+#endif  DFSETTINGS_H
+
